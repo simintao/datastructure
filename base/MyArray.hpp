b22588675c52470051a4ce2e0396d035895036cc
uniqueName1234 <1295006247@qq.com>
2020-10-09 12:28:07 +0800

    array

---
+++ base/MyArray.hpp
@@ -0,0 +1,84 @@
+#pragma once
+#include <iostream>
+using namespace std;
+
+template<class T>
+class MyArray
+{
+public :
+	MyArray(int capacity) {
+		cout << "MyArray的有参构造调用" <<endl;
+		this->m_Capacity = capacity;
+		this->m_Size = 0;
+		this->pAddress = new T[this->m_Capacity];
+	}
+	
+	//拷贝构造
+	MyArray(const MyArray& arr)
+	{
+		cout << "MyArray的拷贝构造调用" <<endl;
+		this->m_Capacity = arr.m_Capacity;
+		this->m_Size = arr.m_Size;
+		this->pAddress = new T[arr.m_Capacity];
+		for (int i = 0; i < this->m_Size; i++) {
+			this->pAddress[i] = arr.pAddress[i];
+		}
+	}
+	//operator= 防止浅拷贝问题
+	MyArray& operator=(const MyArray& arr) 
+	{
+		cout << "MyArray的operator=调用" <<endl;
+		if (this->pAddress != NULL) {
+			delete[] this->pAddress;
+			this->pAddress = NULL;
+			this->m_Capacity = 0;
+			this->m_Size = 0;
+		}
+		this->m_Capacity = arr.m_Capacity;
+		this->m_Size = arr.m_Size;
+		this->pAddress = new T[arr.m_Capacity];
+		for (int i = 0; i < this->m_Size; i++) {
+			this->pAddress[i] = arr.pAddress[i];
+		}
+		return *this;
+	}
+	//尾插法
+	void Push_Back(const T& val) {
+		if (this->m_Capacity == this->m_Size) {
+			return;
+		}
+		this->pAddress[this->m_Size] = val;
+		this->m_Size++;
+	}
+	//尾删法
+	void Pop_Back() {
+		if (this->m_Capacity == 0) {
+			return;
+		}
+		this->m_Size--;
+	}
+	//通过下标方式访问数组中的元素
+	T& operator[](int index) {
+		return this->pAddress[index];
+	}
+	//返回数组容量
+	int getCapacity() {
+		return this->m_Capacity;
+	}
+	//返回数组大小
+	int getSize() {
+		return this->m_Size;
+	}
+
+	~MyArray() {
+		if (this->pAddress != NULL) {
+			cout << "MyArray的析构函数构造调用"<<endl;
+			delete[] this->pAddress;
+			this->pAddress = NULL;
+		}
+	}
+private:
+	T* pAddress;
+	int m_Capacity;
+	int m_Size;
+};
