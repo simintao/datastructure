d65827fefd15e99036e9ff1937c2ebfdfb4003d6
Simin Tao <simintao@126.com>
2020-10-15 22:54:02 +0800

    add shard lib

--- base/HMapTest.cc
+++ base/HMapTest.cc
@@ -6,9 +6,9 @@ using pcl::HMap;
 namespace {
 
 TEST(HMapTest, Ctor) {
-  HMap<std::string, const char *> hmap = {{"1", "test"}};
+  HMap<int, const char *> hmap = {{1, "test"}};
 
-  EXPECT_STREQ(hmap.findKey("1"), "test");
+  EXPECT_STREQ(hmap.findKey(1), "test");
 }
 
 }  // namespace
