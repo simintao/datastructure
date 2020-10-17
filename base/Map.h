fd8eb6d043a514bc588887a4a08a93c03bbaf8ac
Simin Tao <simintao@126.com>
2020-10-17 16:57:59 +0800

    add multimap

--- base/Map.h
+++ base/Map.h
@@ -29,14 +29,10 @@ template <class KEY, class VALUE, class CMP = std::less<KEY>>
 class Map : public absl::btree_map<KEY, VALUE, CMP> {
  public:
   using Base = typename Map::btree_map;
-
-  Map() : absl::btree_map<KEY, VALUE, CMP>() {}
-  using Base::Base;
-  explicit Map(const CMP& cmp) : absl::btree_map<KEY, VALUE, CMP>(cmp) {}
-
   using iterator = typename Base::iterator;
   using citerator = typename Base::const_iterator;
 
+  using Base::Base;
   using Base::begin;
   using Base::cbegin;
   using Base::cend;
@@ -98,39 +94,17 @@ class Map : public absl::btree_map<KEY, VALUE, CMP> {
   bool hasKey(const KEY key) const { return this->find(key) != this->end(); }
 
   // Find the value corresponding to key.
-  VALUE
-  findKey(const KEY key) const {
+  const VALUE value(const KEY key, const VALUE& defaultValue = VALUE()) const {
     auto find_iter = this->find(key);
     if (find_iter != this->end())
       return find_iter->second;
     else
-      return nullptr;
-  }
-  void findKey(const KEY key,
-               // Return Values.
-               VALUE* value, bool* exists) const {
-    auto find_iter = this->find(key);
-    if (find_iter != this->end()) {
-      *value = find_iter->second;
-      *exists = true;
-    } else {
-      *exists = false;
-    }
-  }
-  void findKey(const KEY& key,
-               // Return Values.
-               KEY* map_key, VALUE* value, bool* exists) const {
-    auto find_iter = this->find(key);
-    if (find_iter != this->end()) {
-      *map_key = find_iter->first;
-      *value = find_iter->second;
-      *exists = true;
-    } else {
-      *exists = false;
-    }
+      return defaultValue;
   }
 
-  void insert(const KEY& key, VALUE value) { this->operator[](key) = value; }
+  void insert(const KEY& key, const VALUE& value) {
+    this->operator[](key) = value;
+  }
 
   void deleteContents() {
     Iterator iter(this);
@@ -158,18 +132,194 @@ class Map : public absl::btree_map<KEY, VALUE, CMP> {
    */
   class Iterator {
    public:
-    Iterator() {}
+    Iterator() = default;
     explicit Iterator(Map<KEY, VALUE, CMP>* container) {
-      if (container != nullptr) _iter = container->begin();
+      if (container != nullptr) {
+        _container = container;
+        _iter = container->begin();
+      }
     }
     explicit Iterator(const Map<KEY, VALUE, CMP>& container) {
-      if (container != nullptr) _iter = container->begin();
+      if (container != nullptr) {
+        _container = container;
+        _iter = container->begin();
+      }
     }
     void init(Map<KEY, VALUE, CMP>* container) {
-      container = container;
+      if (container != nullptr) {
+        _container = container;
+        _iter = container->begin();
+      }
+    }
+    void init(const Map<KEY, VALUE, CMP>& container) {
+      if (container != nullptr) {
+        _container = container;
+        _iter = container->begin();
+      }
+    }
+    bool hasNext() { return _iter != _container->end(); }
+    VALUE next() { return _iter++->second; }
+    void next(KEY* key, VALUE* value) {
+      *key = _iter->first;
+      *value = _iter->second;
+      _iter++;
+    }
+
+    inline const KEY& key() const { return _iter->first(); }
+    inline const VALUE& value() const { return _iter->second(); }
+    inline const VALUE& operator*() const { return _iter->value(); }
+    inline const VALUE& operator->() const { return &_iter->value(); }
+    inline bool operator==(const Iterator& o) const { return _iter == o._iter; }
+    inline bool operator!=(const Iterator& o) const { return _iter != o._iter; }
+
+   private:
+    Map<KEY, VALUE, CMP>* _container = nullptr;
+    typename Map<KEY, VALUE, CMP>::iterator _iter;
+  };
+  friend class Iterator;
+
+  class ConstIterator {
+   public:
+    ConstIterator() = default;
+    explicit ConstIterator(const Map<KEY, VALUE, CMP>* container) {
+      if (container != nullptr) {
+        _container = container;
+        _iter = container->begin();
+      }
+    }
+    explicit ConstIterator(const Map<KEY, VALUE, CMP>& container) {
+      if (container != nullptr) {
+        _container = container;
+        _iter = container->begin();
+      }
+    }
+
+    void init(const Map<KEY, VALUE, CMP>* container) {
+      if (container != nullptr) {
+        _container = container;
+        _iter = container->begin();
+      }
+    }
+    void init(const Map<KEY, VALUE, CMP>& container) {
+      if (container != nullptr) {
+        _container = container;
+        _iter = container->begin();
+      }
+    }
+    bool hasNext() {
+      return _container != nullptr && _iter != _container->end();
+    }
+    VALUE next() { return _iter++->second; }
+    void next(KEY* key, VALUE* value) {
+      *key = _iter->first;
+      *value = _iter->second;
+      _iter++;
+    }
+
+    inline const KEY& key() const { return _iter->first(); }
+    inline const VALUE& value() const { return _iter->second(); }
+    inline const VALUE& operator*() const { return _iter->value(); }
+    inline const VALUE& operator->() const { return &_iter->value(); }
+    inline bool operator==(const ConstIterator& o) const {
+      return _iter == o._iter;
+    }
+    inline bool operator!=(const ConstIterator& o) const {
+      return _iter != o._iter;
+    }
+
+   private:
+    Map<KEY, VALUE, CMP>* _container = nullptr;
+    typename Map<KEY, VALUE, CMP>::citerator _iter;
+  };
+
+  friend class ConstIterator;
+
+  class KeyIterator {
+   public:
+    KeyIterator() = default;
+    explicit KeyIterator(const Map<KEY, VALUE, CMP>* container) {
+      if (container != nullptr) _iter = container->begin();
+    }
+
+    void init(const Map<KEY, VALUE, CMP>* container) {
       if (container != nullptr) _iter = container->begin();
     }
     void init(const Map<KEY, VALUE, CMP>& container) {
+      if (container != nullptr) _iter = container->begin();
+    }
+
+    bool hasNext() {
+      return _container != nullptr && _iter != _container->end();
+    }
+    KEY next() { return _iter++->first; }
+    void next(KEY* key, VALUE* value) {
+      *key = _iter->first;
+      *value = _iter->second;
+      _iter++;
+    }
+
+    const KEY& operator*() const { return _iter.key(); }
+    bool operator==(KeyIterator o) const { return _iter == o.i; }
+    bool operator!=(KeyIterator o) const { return _iter != o.i; }
+
+   private:
+    Map<KEY, VALUE, CMP>* _container;
+    typename Map<KEY, VALUE, CMP>::citerator _iter;
+  };
+
+  friend class KeyIterator;
+};
+
+/**
+ * @brief The multimap is an ordered associative container.
+ *
+ * It allows multiple elements with equivalent keys.
+ */
+template <typename KEY, typename VALUE, typename CMP = std::less<KEY>>
+class Multimap : public absl::btree_multimap<KEY, VALUE, CMP> {
+  using Base = typename Multimap::btree_multimap;
+  using iterator = typename Base::iterator;
+  using citerator = typename Base::const_iterator;
+  using ValueType = typename Base::value_type;
+
+ public:
+  using Base::Base;
+  using Base::begin;
+  using Base::cbegin;
+  using Base::cend;
+  using Base::clear;
+  using Base::contains;
+  using Base::count;
+  using Base::emplace;
+  using Base::empty;
+  using Base::end;
+  using Base::equal_range;
+  using Base::erase;
+  using Base::extract;
+  using Base::find;
+  using Base::insert;
+  using Base::merge;
+  using Base::size;
+  using Base::swap;
+
+  void insert(const KEY& key, const VALUE& value) {
+    insert(ValueType(key, value));
+  }
+
+  class Iterator {
+   public:
+    Iterator() = default;
+    explicit Iterator(Multimap<KEY, VALUE, CMP>* container) {
+      if (container != nullptr) _iter = container->begin();
+    }
+    explicit Iterator(const Multimap<KEY, VALUE, CMP>& container) {
+      if (container != nullptr) _iter = container->begin();
+    }
+    void init(Multimap<KEY, VALUE, CMP>* container) {
+      container = container;
+      if (container != nullptr) _iter = container->begin();
+    }
+    void init(const Multimap<KEY, VALUE, CMP>& container) {
       if (container != nullptr) _iter = container->begin();
     }
     bool hasNext() { return _iter != _container->end(); }
@@ -181,24 +331,24 @@ class Map : public absl::btree_map<KEY, VALUE, CMP> {
     }
 
    private:
-    Map<KEY, VALUE, CMP>* _container;
-    typename Map<KEY, VALUE, CMP>::iterator _iter;
+    Multimap<KEY, VALUE, CMP>* _container;
+    typename Multimap<KEY, VALUE, CMP>::iterator _iter;
   };
   friend class Iterator;
 
   class ConstIterator {
    public:
-    ConstIterator() {}
-    explicit ConstIterator(const Map<KEY, VALUE, CMP>* container) {
+    ConstIterator() = default;
+    explicit ConstIterator(const Multimap<KEY, VALUE, CMP>* container) {
       if (container != nullptr) _iter = container->begin();
     }
-    explicit ConstIterator(const Map<KEY, VALUE, CMP>& container) {
+    explicit ConstIterator(const Multimap<KEY, VALUE, CMP>& container) {
       if (container != nullptr) _iter = container->begin();
     }
-    void init(const Map<KEY, VALUE, CMP>* container) {
+    void init(const Multimap<KEY, VALUE, CMP>* container) {
       if (container != nullptr) _iter = container->begin();
     }
-    void init(const Map<KEY, VALUE, CMP>& container) {
+    void init(const Multimap<KEY, VALUE, CMP>& container) {
       if (container != nullptr) _iter = container->begin();
     }
     bool hasNext() {
@@ -223,27 +373,45 @@ class Map : public absl::btree_map<KEY, VALUE, CMP> {
     }
 
    private:
-    Map<KEY, VALUE, CMP>* _container;
-    typename Map<KEY, VALUE, CMP>::citerator _iter;
+    Multimap<KEY, VALUE, CMP>* _container;
+    typename Multimap<KEY, VALUE, CMP>::citerator _iter;
   };
-
   friend class ConstIterator;
 
   class KeyIterator {
    public:
     KeyIterator() = default;
-    explicit KeyIterator(ConstIterator o) : i(o) {}
+    explicit KeyIterator(const Multimap<KEY, VALUE, CMP>* container) {
+      if (container != nullptr) _iter = container->begin();
+    }
 
-    const KEY& operator*() const { return i.key(); }
-    const KEY* operator->() const { return &i.key(); }
-    bool operator==(KeyIterator o) const { return i == o.i; }
-    bool operator!=(KeyIterator o) const { return i != o.i; }
+    void init(const Multimap<KEY, VALUE, CMP>* container) {
+      if (container != nullptr) _iter = container->begin();
+    }
+    void init(const Multimap<KEY, VALUE, CMP>& container) {
+      if (container != nullptr) _iter = container->begin();
+    }
+
+    const KEY& operator*() const { return _iter.key(); }
+    const KEY* operator->() const { return &_iter.key(); }
+    bool operator==(KeyIterator o) const { return _iter == o.i; }
+    bool operator!=(KeyIterator o) const { return _iter != o.i; }
+
+    bool hasNext() {
+      return _container != nullptr && _iter != _container->end();
+    }
+    KEY next() { return _iter++->first; }
+    void next(KEY* key, VALUE* value) {
+      *key = _iter->first;
+      *value = _iter->second;
+      _iter++;
+    }
 
    private:
-    ConstIterator i;
+    Multimap<KEY, VALUE, CMP>* _container;
+    typename Multimap<KEY, VALUE, CMP>::citerator _iter;
   };
 
   friend class KeyIterator;
 };
-
 }  // namespace pcl
--- base/MapTest.cc
+++ base/MapTest.cc
@@ -2,13 +2,26 @@
 #include "gtest/gtest.h"
 
 using pcl::Map;
+using pcl::Multimap;
 
 namespace {
-
 TEST(MapTest, Ctor) {
   Map<int, const char *> bmap = {{1, "test"}};
 
-  EXPECT_STREQ(bmap.findKey(1), "test");
+  EXPECT_STREQ(bmap.value(1), "test");
+}
+
+TEST(MultimapTest, Ctor) {
+  Multimap<int, std::string> mmap = {{1, "test"}};
+
+  std::cout << mmap.size() << std::endl;
+
+  mmap.insert(1, "test1");
+  std::cout << mmap.size() << std::endl;
+
+  auto values = mmap.find(1);
+
+  std::cout << values->second << std::endl;
 }
 
 }  // namespace
