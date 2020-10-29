431cae9ce2dadba361324ccfa33a22479ee4edc2
AlbertTao <simintao@gmail.com>
2020-10-29 12:13:49 +0800

    fix cmake option syntax

--- main.cc
+++ main.cc
@@ -1,10 +1,18 @@
 #include <iostream>
+#include "base/include/HMap.h"
 #include "base/include/Map.h"
 
+using pcl::HMap;
 using pcl::Map;
+
 int main() {
+  HMap<int, const char *> hmap{{1, "a"}, {1, "b"}};
+  for (auto p = hmap.cbegin(); p != hmap.cend(); p++) {
+    std::cout << p->first << " " << p->second << std::endl;
+  }
+
   Map<int, const char *> bmap{{1, "a"}, {1, "b"}};
-  for (auto p = bmap.crbegin(); p != bmap.crend(); p++) {
+  for (auto p = bmap.cbegin(); p != bmap.cend(); p++) {
     std::cout << p->first << " " << p->second << std::endl;
   }
 }
