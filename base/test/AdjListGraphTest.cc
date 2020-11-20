bc0a29d8d33e4261f22e2d5ab73b2d67083699bd
uniqueName1234 <1295006247@qq.com>
2020-11-20 09:59:29 +0800

    add test

---
+++ base/test/AdjListGraphTest.cc
@@ -0,0 +1,19 @@
+
+#include <iostream>
+
+#include "AdjListGraphV.h"
+#include "gtest/gtest.h"
+using namespace pcl;
+TEST(AdjGraph, test) {
+  Graph graph(7);
+  graph.createGraph(0, 1, 3);
+  graph.createGraph(0, 2, 5);
+  graph.createGraph(2, 3, 4);
+  graph.createGraph(2, 4, 2);
+  graph.createGraph(4, 5, 3);
+  graph.createGraph(5, 6, 3);
+  graph.printAdjVector();
+  graph.insertEdge(2, 5, 5);
+  graph.printAdjVector();
+  graph.deleteEdge(2, 5);
+}
